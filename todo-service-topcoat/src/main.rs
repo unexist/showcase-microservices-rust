9e5a2f5f05ce7be77f782e2cd44f0f5b1ce21943
Christoph Kappel <christoph@unexist.dev>
2026-08-14 09:49:49 +0200

    Added copyleft

--- todo-service-topcoat/src/main.rs
+++ todo-service-topcoat/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+///
+/// @package Showcase-Microservices-Rust
+///
+/// @file Main entry
+/// @copyright 2026-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv2.
+/// See the file LICENSE for details.
+///
+
+use topcoat::{
+    Result,
+    router::{Router, RouterBuilderDiscoverExt, page},
+    view::{component, view},
+};
+
+#[tokio::main]
+async fn main() {
+    topcoat::start(Router::builder().discover().build()).await.unwrap();
+}
+
+#[page("/")]
+async fn home() -> Result {
+    view! {
+        <!DOCTYPE html>
+        <html>
+            <body>
+                hello(name: "World")
+            </body>
+        </html>
+    }
+}
+
+#[component]
+async fn hello(name: &str) -> Result {
+    view! { <h1>"Hello, " (name) "!"</h1> }
 }
